b6e86cec6a902c7eee217cd1aefc6c6c05eb0071
andrewvdelarosa <andrewvdelarosa@gmail.com>
2020-08-18 11:44:31 -0700

    getitems

--- src/api.js
+++ src/api.js
@@ -12,7 +12,20 @@ function createItem(name){
   return fetch(`${BASE_URL}/items`, {method:'POST', headers:{'Content-Type': 'application/json'}, body: newItem});
 }
 
+function updateItem(id,updateData){
+  const newData = JSON.stringify(updateData);
+  return fetch(`${BASE_URL}/items/${id}`, {
+    method:'PATCH', 
+    headers:{
+      'Content-Type': 'application/json',
+    }, 
+    body: newData
+  });
+};
+
 export default {
   getItems,
   createItem,
+  updateItem
 };
+
--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ import './index.css';
 import shoppingList from './shopping-list';
 
 const main = function () {
+
+  api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    const item = items[0];
+    return api.updateItem(item.id, { name: 'foobar' });
+  })
+  .then(res => res.json())
+  .then(() => console.log('updated!'));
+
   api.getItems()
     .then(res => res.json())
     .then((items) => {
