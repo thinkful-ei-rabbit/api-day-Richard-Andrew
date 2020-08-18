f173c6a3c89dad43e3f3948dcd09fcbdaca076b9
Richard Carmen Scott <RichardCarmenScott@gmail.com>
2020-08-18 11:47:33 -0400

    added npm install

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import './index.css';
 import shoppingList from './shopping-list';
 
 const main = function () {
+  fetch('https://thinkful-list-api.herokuapp.com/ei-student/items')
+    .then(res => res.json())
+    .then(data => console.log(data));
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
